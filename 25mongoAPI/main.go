b3cb8d22eca3f1554288e7ac5dbc46bc407511aa
Slutishboy <sohomsaha361@gmail.com>
2024-09-21 02:47:01 +0530

    Controller-Setup

---
+++ 25mongoAPI/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi from Mongo Connection")
+	//sohomsahacse2020
+	//Fv0RU1NbAcPnfe0r
+	//mongodb+srv://sohomsahacse2020:<db_password>@cluster0.6dx48.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+}
